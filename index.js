027f09247a94976f109329bf2d1320f460dc0afe
Muhammad Rivki <muhammad.rivki@outlook.com>
2017-05-13 23:46:40 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+const alfy = require('alfy');
+const trending = require('trending-github');
+
+let [language, period] = alfy.input.split(' ')
+if (!period) period = 'daily'
+
+if (alfy.cache.get('repos')) {
+  alfy.output(alfy.cache.get('repos'))
+} else {
+  trending(period, alfy.input)
+    .then(repos => {
+      const output = repos.map(repo => ({
+        title: `${repo.author}/${repo.name}`,
+        subtitle: repo.description,
+        arg: repo.href,
+        mods: {
+          alt: {
+            subtitle: `Language: ${repo.language}, stars: ${repo.stars}, fork: ${repo.forks}`
+          }
+        },
+        quicklookurl: repo.href
+      }));
+
+      alfy.cache.set('repos', output, {
+        maxAge: 10000
+      })
+      alfy.output(output)
+    })
+}
---
+++ readme.md
@@ -0,0 +1,22 @@
+# alfred-github-explorer [![Build Status](https://travis-ci.org/mikqi/alfred-github-explorer.svg?branch=master)](https://travis-ci.org/mikqi/alfred-github-explorer)
+
+> Get trending repo on GitHub
+
+
+## Install
+
+```
+$ npm install --global alfred-github-trending
+```
+
+*Requires [Node.js](https://nodejs.org) 4+ and the Alfred [Powerpack](https://www.alfredapp.com/powerpack/).*
+
+
+## Usage
+
+In Alfred, type `gtr`, <kbd>Enter</kbd>, and your query.
+
+
+## License
+
+MIT © [Muhammad Rivki](https://rivki.js.org)
