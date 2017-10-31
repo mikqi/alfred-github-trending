c412e5bdca9387b67cca141305f8d3c7a2a6a5cd
Muhammad Rivki <muhammad.rivki@outlook.com>
2017-10-31 22:05:27 +0700

    fix xo

--- test.js
+++ test.js
@@ -1,18 +1,17 @@
-import test from 'ava';
-import alfyTest from 'alfy-test';
+import test from 'ava'
+import alfyTest from 'alfy-test'
 // import trending from 'trending-github';
 
 test(async t => {
-  const alfy = alfyTest();
-  const result = await alfy('Rainbow');
-  delete result[0].mods;
+  const alfy = alfyTest()
+  const result = await alfy('Rainbow')
+  delete result[0].mods
   t.deepEqual([result[0]], [
     {
-      title: 'yandex/gixy',
-      subtitle: 'Nginx configuration static analyzer',
-      quicklookurl: 'https://github.com/yandex/gixy',
-      arg: 'https://github.com/yandex/gixy'
+      title: 'Alimei/hangzhouYunQi2017ppt',
+      subtitle: null,
+      quicklookurl: 'https://github.com/Alimei/hangzhouYunQi2017ppt',
+      arg: 'https://github.com/Alimei/hangzhouYunQi2017ppt'
     }
-  ]);
-});
-
+  ])
+})
